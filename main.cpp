df3eab6ad4c0bfd331b7587fef0105fade86e444
ikumei <92413582+ikumei831@users.noreply.github.com>
2022-10-07 16:03:33 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -6,60 +9,98 @@ using namespace std;
 int row,col;
 void number();
 char a;
-char b;
+int b;
+int ranx,rany;
+int bomb;
+int flag=0;
 int main(int argc, char** argv) {
+	srand((unsigned)time(NULL));
+	while(true){
+		printf("col row\n");
+		scanf("%d %d",&col,&row);
+		if(col<=10&&row<=10){
+			break;
+		}
+		else printf("out of value\ninsert lower than 10*10\n");
+	}
 	
-	printf("col row\n");
-	scanf("%d %d",&col,&row);
 	int *col1=new int(col+1);	
 	int *row1=new int(row+1);
 	int surface[*col1][*row1];
+
+	
+	
+	
+	
 	
 	for (int x=0;x<*col1;x++){
 		for (int y=0;y<*row1;y++){
-			surface[x][y]=x*(*col1)+y;
-			
+			surface[x][y]=x*(*col1)+y;	
 			if(x==0)printf("%d ",surface[x][y]);
-			if(surface[x][y]==9999)printf("x");
-			else printf(" ");
+			
+			
 		}
 		printf("\n");
-		printf("%c ",x+65);
+		if(x<*col1-1)printf("%c ",x+65);
 	}
+	printf("\n");
 	
+
 	while(true){
 		
 		
 		
 		printf("\ncol row\n");
 		fflush(stdin);
-		scanf("%c%c",&a,&b);
-		printf("val:%c%c\n",a,b);	
+		scanf("%c%d",&a,&b);
+		printf("val:%c%d\n",a,b);	
 		
-		
+		b-=1;
 		
 		
 		if(a<='A'&&a>='Z'){
-			b-=48;
 			a-=64;
 			surface[a][b]=9999; 
 		}
 		else if(a>='a'&&a<='z'){
-			b-=48;
 			a-=96;
 			surface[a][b]=9999; 
 		}
+		
+		printf("%d %d\n\n",a,b+1);
+		
+		if(flag==0){
+
+			bomb=col*row/10;
+			if (bomb==0)bomb=1;
+			
+			for(int i=0;i<=bomb;i++){
+				ranx=rand()%bomb+1;
+				rany=rand()%bomb;
+				if(surface[ranx][rany]!=6666||9999){
+					surface[ranx][rany]=6666;
+					printf("%d %d\n",ranx,rany+1);
+				}
+				else i-=1;
+				printf("%d \n",i);
+			}
+			flag++;
+		}
+		printf("\n\n");
 	
-		printf("%d %d\n",a,b);
+		
 		for (int x=0;x<*col1;x++){
-			for (int y=0;y<*row1;y++){
+			for (int y=0;y<=*row1;y++){
 				
-				if(x==0)printf("%d ",surface[x][y]);
-				if(surface[x][y]==9999)printf("x ");
-				else printf("");
+				if(y<*row1){
+					if(x==0)printf("%d ",surface[x][y]);
+					else if(surface[x][y]==9999)printf("x ");
+					else if(surface[x][y]==6666)printf("o ");
+					else if(y<*row1-1)printf("  ");
+				}
 			}
 			printf("\n");
-			printf("%c ",x+65);
+			if(x<*col1-1)printf("%c ",x+65);
 		}
 		printf("\n");
 	
